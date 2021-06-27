b6c3c5f6efceb97a9b116a15855f8b052f9b2510
yuji moriya <voyager.nasa0928@gmail.com>
2021-06-27 16:32:05 +0900

    記事一覧画面の作成

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
