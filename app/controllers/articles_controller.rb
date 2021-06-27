faf51896fa9bb355ca24d2891a8257b4d5ab5b39
yuji moriya <voyager.nasa0928@gmail.com>
2021-06-27 20:19:27 +0900

    記事編集画面の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -11,20 +13,33 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path, notice: "ブログを作成しました"
+      redirect_to articles_path, notice: t("notice.article_create")
     else
       render :new
     end
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to articles_path, notice: t("notice.article_update")
+    else
+      render :edit
+    end
   end
 
   private
-
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
