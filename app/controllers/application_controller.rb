fc1764a25d971ac1145e88fa8ee1938014306ed3
yuji moriya <voyager.nasa0928@gmail.com>
2021-07-18 19:27:11 +0900

    プロフィール編集画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :image_cache, :introduction])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :image, :image_cache, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :image_cache, :introduction])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  mount_uploader :image, ImageUploader
+
   def follow!(other_user)
     active_relationships.create!(followed_id: other_user.id)
   end
