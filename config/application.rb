fbfb92e6baf44fdcf8bcba534fcd171e43f36f87
yuji moriya <voyager.nasa0928@gmail.com>
2021-08-09 23:53:39 +0900

    アイコンの表示修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Mitano
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.generators do |g|
       g.assets false
       g.helper false
