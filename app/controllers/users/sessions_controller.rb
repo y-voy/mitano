fd926f1c319d143b78a8b373e8463a7c4914a273
yuji moriya <voyager.nasa0928@gmail.com>
2021-08-15 23:00:50 +0900

    ゲストログイン機能修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path, notice: t('view.sign_in_as_guest')
   end
 end
